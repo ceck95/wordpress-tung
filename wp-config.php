48bc719c78ee5f9ea87788821710744bcc227951
nhutdev <tranvannhut4495@gmail.com>
2017-03-24 14:28:28 +0700

    update sql

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'bds');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'nhutnhut');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
